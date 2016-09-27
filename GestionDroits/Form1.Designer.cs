4d37e4544bef7e76430bace206487a62378fc537
Nicolas Le Gall <contact@nlegall.fr>
2016-09-27 10:14:39 +0200

    Fix label

--- GestionDroits/Form1.Designer.cs
+++ GestionDroits/Form1.Designer.cs
@@ -143,9 +143,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(578, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 24);
+            this.label1.Size = new System.Drawing.Size(197, 24);
             this.label1.TabIndex = 12;
-            this.label1.Text = "Responsable du partage :";
+            this.label1.Text = "Nom du responsable :";
             // 
             // responsable
             // 
